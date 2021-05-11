f17b904d9e5344a72e3649913d4bc06b630628c7
Jasper <80272820+Code-Jap@users.noreply.github.com>
2021-05-11 23:33:00 +0200

    Add files via upload

---
+++ JavaScript_4/script.js
@@ -0,0 +1,87 @@
+const age = 26;
+
+if (age >= 18){
+
+    console.log("You may enter");
+
+} else {
+
+    console.log("You can not enter");
+
+}
+
+
+const isFemale = true;
+
+if (isFemale){
+
+    console.log("You're a female");
+
+} else {
+
+    console.log("you're a guy");
+}
+
+
+const driverSatus = "bob";
+
+if (driverSatus == "bob"){
+
+    console.log("You can drive");
+
+} else {
+
+    console.log("You can not drive");
+}
+
+
+
+if (age >= 18 && age <= 25){
+
+    console.log("Je krijgt 50% korting");
+
+} else{
+
+    console.log("Geen korting voor jou")
+}
+
+
+
+
+const namePerson = "Stefanie";
+
+if (namePerson == "Bram" || namePerson == "Sarah"){
+
+    console.log("Gratis bier!");
+
+} else {
+
+    console.log("Helaas, geen gratis bier voor jou");
+
+}
+
+
+
+const totalAmount = 25;
+
+
+if (totalAmount > 100){
+
+    console.log("Gratis champagne!");
+
+} else if (totalAmount > 50){
+
+    console.log("Gratis nacho's!");
+
+} else if (totalAmount > 25){
+
+    console.log("Gratis bitterballen!");
+
+
+} else {(totalAmount <= 25 )
+
+    console.log("Niks gratis voor jou!");
+}
+
+
+
